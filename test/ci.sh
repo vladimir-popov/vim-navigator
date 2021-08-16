1b5c38aed309fca99bec0fe3343f8203ef9d6d09
Vladimir Popov <vladimir@dokwork.ru>
2021-08-16 10:26:50 +0300

    Fix ci.sh

--- test/ci.sh
+++ test/ci.sh
@@ -4,6 +4,9 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' 
 
+VADER_RESULTS=("${GREEN}Results:$NC")
+RESULT=0
+
 for test in *.vader
 do 
   vim -EsNu vimrc.ci -c "Vader! $test"
@@ -11,7 +14,6 @@ do
   if [[ $? == 0 ]]
   then
     VADER_RESULTS+=("$GREEN + SUCCESS: $test $NC")
-    RESULT=0
   else
     VADER_RESULTS+=("$RED - FAIL: $test $NC")
     RESULT=1
