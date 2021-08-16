e6e0ee95f2490cdbe72f9564e1e2f32fd75699ca
Vladimir Popov <vladimir@dokwork.ru>
2021-08-16 10:46:14 +0300

    Test

--- test/ci.sh
+++ test/ci.sh
@@ -7,21 +7,4 @@ NC='\033[0m'
 VADER_RESULTS=("${GREEN}Results:$NC")
 RESULT=0
 
-pwd
-
-for test in ./test/*.vader
-do 
-  vim -EsNu ./test/vimrc.ci -c "Vader! $test"
-
-  if [[ $? == 0 ]]
-  then
-    VADER_RESULTS+=("$GREEN + SUCCESS: $test $NC")
-  else
-    VADER_RESULTS+=("$RED - FAIL: $test $NC")
-    RESULT=1
-  fi
-done
-
-printf '%b\n' "${VADER_RESULTS[@]}"
-
-exit $RESULT
+vim -EsNu ./test/vimrc.ci -c "Vader! ./test/render.vader"
