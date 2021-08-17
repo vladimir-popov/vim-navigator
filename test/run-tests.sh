2d936327f948d6dcd13b63260d4535c6fe69bc11
Vladimir Popov <vladimir@dokwork.ru>
2021-08-17 15:24:00 +0300

    Improve run-tests.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -21,7 +21,29 @@
 # 'https://github.com/junegunn/vader.vim' will cloned 
 # to the temp directory which will be used as the 
 # `VADER_PATH`.
-# =====================================================
+# ====================================================
+
+
+# got to the directory with this script (./test/):
+cd $(dirname ${BASH_SOURCE[0]})
+
+# path to the Vader plugin:
+VADER_PATH=${VADER_PATH:="${HOME}/.vim/plugged/vader.vim/"}
+
+if ! [[ -d $VADER_PATH ]]
+then
+  VADER_PATH=$(mktemp -d)
+  echo 'Clone the vader.vim to the '$VADER_PATH
+  git clone --depth=1 'https://github.com/junegunn/vader.vim.git' "$VADER_PATH"
+  echo 'cloned.'
+fi
+        
+
+# path to the directory with plugin for test:
+PLUGIN_PATH=${PLUGIN_PATH:='../'}
+
+# regexp to select tests to run
+[[ -z "$1" ]] && VADER_TESTS=*.vader || VADER_TESTS="$1"
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -29,15 +51,14 @@ NC='\033[0m'
 
 VADER_RESULTS=("${GREEN}Results:$NC")
 RESULT=0
+VIMRC="filetype off | set rtp+=$VADER_PATH | set rtp+=$PLUGIN_PATH | filetype plugin indent on | syntax enable"
+
+echo $VIMRC
 
 function run() {
-  vim -EsNu <(cat << EOF
-    filetype off
-    set rtp+=$VADER_PATH
-    set rtp+=$PLUGIN_PATH
-    filetype plugin indent on
-    syntax enable
-  ) -c "Vader! $1"
+  echo "Run $1"
+
+  vim -Nu <(echo $VIMRC) -c "Vader! $1"
 
   if [[ $? == 0 ]]
   then
@@ -48,35 +69,7 @@ function run() {
   fi
 }
 
-function clone_vader() { 
-  VADER_PATH=$(mktemp -d)
-  echo 'Clone the vader.vim to the '$VADER_PATH
-  git clone --depth=1 'https://github.com/junegunn/vader.vim.git' "$VADER_PATH" 
-}  
-
-# got to the directory with this script (./test/):
-cd $(dirname ${BASH_SOURCE[0]})
-
-# path to the Vader plugin:
-VADER_PATH=${VADER_PATH:='~/.vim/plugged/vader.vim/'}
-
-[ -d $VADER_PATH ] && [ ! -L $VADER_PATH ] || clone_vader()
-
-# path to the directory with plugin for test:
-PLUGIN_PATH=${PLUGIN_PATH:='../'}
-
-# regexp to select tests to run
-[[ -z "$1" ]] && VADER_TESTS=*.vader || VADER_TESTS="$1"
-
-for test in $VADER_TESTS
-do 
-  if [[ "$VADER_TESTS" == '*.vader' ]] 
-  then
-    run $test
-  else
-    run $test
-  fi  
-done
+for test in $VADER_TESTS; do run $test; done
 
 printf '%b\n' "${VADER_RESULTS[@]}"
 
